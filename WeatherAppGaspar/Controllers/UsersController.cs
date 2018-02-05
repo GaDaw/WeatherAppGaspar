bea77fb3bc69b0aa4d77dc64a5129133160602ee
aws <aws@DESKTOP-4G8L7AE>
2018-02-05 13:49:13 +0100

    Consultar datos clima actual en Api Externa y guardarlos en el campo Metereology del usuario cuando se crea un usuario nuevo. Crear modelo para deserializar los datos de la Api

--- WeatherAppGaspar/Controllers/UsersController.cs
+++ WeatherAppGaspar/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using WeatherAppGaspar.Data;
 using WeatherAppGaspar.Models;
 
@@ -58,6 +60,20 @@ namespace WeatherAppGaspar.Controllers
         {
             if (ModelState.IsValid)
             {
+
+                // Hacemos una llamada a la Api de Openweather obteniendo datos metereologicos para las coordenadas dadas y las guardamos algunos en el campo Metereology del usuario
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?lat={user.Latitude}&lon={user.Longitude}&units=metric&APPID=747935212617bea48b0bbb48e8f6a455");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<DeserializeExternalApi>(stringResult);
+                    user.Meteorology = ("Ciudad: " + rawWeather.Name + "  Temperatura: " + rawWeather.Main.Temp + "  Presión: " + rawWeather.Main.Pressure + "  Humidity: " + rawWeather.Main.Humidity);
+
+                }
+
                 _context.Add(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -97,6 +113,19 @@ namespace WeatherAppGaspar.Controllers
             {
                 try
                 {
+                    // Hacemos una llamada a la Api de Openweather obteniendo datos metereologicos para las coordenadas dadas y las guardamos algunos en el campo Metereology del usuario
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("http://api.openweathermap.org");
+                        var response = await client.GetAsync($"/data/2.5/weather?lat={user.Latitude}&lon={user.Longitude}&units=metric&APPID=747935212617bea48b0bbb48e8f6a455");
+                        response.EnsureSuccessStatusCode();
+
+                        var stringResult = await response.Content.ReadAsStringAsync();
+                        var rawWeather = JsonConvert.DeserializeObject<DeserializeExternalApi>(stringResult);
+                        user.Meteorology = ("Ciudad: " + rawWeather.Name + "  Temperatura: " + rawWeather.Main.Temp + "  Presión: " + rawWeather.Main.Pressure + "  Humidity: " + rawWeather.Main.Humidity);
+
+                    }
+
                     _context.Update(user);
                     await _context.SaveChangesAsync();
                 }
---
+++ WeatherAppGaspar/Models/DeserializeExternalApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherAppGaspar.Models
+{
+    public class DeserializeExternalApi
+    {
+        public string Name { get; set; }
+        public Main Main { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+    }
+}
+
