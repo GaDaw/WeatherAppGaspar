bc8c7d6ea8ed831864fe9f324eee76122c5c8a68
aws <aws@DESKTOP-4G8L7AE>
2018-02-05 14:06:09 +0100

    Crear nuestra API que muestre los nombres de usuario, coordenadas, metereology y forecast. Añadir API al menú de la aplicación

---
+++ WeatherAppGaspar/Controllers/APIController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeatherAppGaspar.Data;
+using WeatherAppGaspar.Models;
+
+namespace WeatherAppGaspar.Controllers
+{
+    [Produces("application/json")]
+    [Route("/API")]
+    public class APIController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public APIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiWeather
+        [HttpGet]
+        // Esta API muestra los nombres de usuarios, coordenadas, metereología y forecast. El resto de campos no se serializan tal como está definido el modelo User
+        public IEnumerable<User> GetUserWeather()
+        {
+            return _context.User;
+        }
+
+        // GET: api/ApiWeather/5
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetUserWeather([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userWeather = await _context.User.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (userWeather == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userWeather);
+        }
+    }
+}
