49b755f61bcedc1a075af43bb49aea7fdcf7d734
aws <aws@DESKTOP-4G8L7AE>
2018-02-05 12:29:24 +0100

    Agregar archivos de proyecto.

---
+++ WeatherAppGaspar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherAppGaspar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
